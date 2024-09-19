399e73a36281b05bfe3142b614c423b2c7051719
Luiz Henrique <7henrique18@gmail.com>
2024-09-19 20:50:15 -0300

    feat(config): Add test coverage configuration for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
 })
