26c97efc4ae01782af342fbe237736d6fc12e61b
Luiz Henrique <7henrique18@gmail.com>
2024-10-01 10:29:46 -0300

    feat(test): add makeHost in increment referral link click count test.

--- src/app/functions/increment-referral-link-click-count.spec.ts
+++ src/app/functions/increment-referral-link-click-count.spec.ts
@@ -3,7 +3,8 @@ import { isLeft, isRight, unwrapEither } from '@/core/either'
 import { ReferralLinkNotFound } from '../errors/referral-link-not-found'
 import { incrementReferralLinkCount } from './increment-referral-link-click-count'
 import { makeReferralLink } from '@/test/factories/make-referral-link'
-import { makeEvent } from '@/test/factories/make-event'
+import { makeActiveEvent, makeEvent } from '@/test/factories/make-event'
+import { makeHost } from '@/test/factories/make-host'
 
 describe('increment referral link click count', () => {
   it('should be able to return an error if the referral link is not found', async () => {
@@ -16,8 +17,9 @@ describe('increment referral link click count', () => {
     expect(unwrapEither(sut)).toBeInstanceOf(ReferralLinkNotFound)
   })
 
-  it('should be to increment click count and return updated referral link', async () => {
-    const event = await makeEvent()
+  it('should be able to increment click count', async () => {
+    const host = await makeHost()
+    const event = await makeActiveEvent({ host_id: host.id })
 
     const { token } = await makeReferralLink({
       event_id: event.id,
