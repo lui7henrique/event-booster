26740dfe3583d474d459369161c970a667914bcd
Luiz Henrique <7henrique18@gmail.com>
2024-10-02 19:34:38 -0300

    feat(server.cjs): add fastify plugins and Zod library functionality

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url(),
-  JWT_SECRET: z.string().optional(),
+  JWT_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
