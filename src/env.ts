ffe6e7126baecee1aa12990ac22d66fc891e2b59
Luiz Henrique <7henrique18@gmail.com>
2024-10-02 19:52:53 -0300

    refactor(env): remove unnecessary URL validation from REDIS_URL field

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url(),
-  REDIS_URL: z.string().url(),
+  REDIS_URL: z.string(),
   JWT_SECRET: z.string(),
 })
 
